6e4d5d35d2278083d35b4b764718a3a2e338566f
PyrHar <Vesihiisi14@outlook.com>
2025-02-25 18:16:12 +0000

    made comments to the code for reviewers help

--- library.py
+++ library.py
@@ -1,6 +1,5 @@
 import sys
 
-
 class Book:
     def __init__(self, name, author, isbn, year):
         self.name = name
@@ -11,7 +10,7 @@ class Book:
     def __str__(self):
         return f"{self.name}/{self.author}/{self.isbn}/{self.year}"
 
-
+# Checking if library exists and if not creating one
 def load_library(file_path):
     books = []
     try:
@@ -24,24 +23,28 @@ def load_library(file_path):
         print(f"There is no '{file_path}', starting a new one.")
     return books
 
-
+#Lambda to extract the year from the book
+#To keep them in order
 def save_books(file_path, books):
     with open(file_path, "w") as file:
         for book in sorted(books, key=lambda b: b.year):
             file.write(f"{book}\n")
 
-
+# Checking if there are books stored inside text files
+# If yes then displaying them into the CMD
 def show_books(books):
     if not books:
         print("\nNo books yet, lets add one!")
         return
-
+#Prints current books in the file if there is any
+#Also put in order from the publishing year
     print("\nCurrent Books:")
     for book in sorted(books, key=lambda b: b.year):
         print(f"{book.name} by {book.author}, {book.year} (ISBN: {book.isbn})")
     print()
 
-
+#Function to add the book where it asks required info
+#All information is required at this stage
 def add_book():
     print("\nAdd a new book:")
     name = input("Title: ").strip()
@@ -55,15 +58,17 @@ def add_book():
         print("Please enter all required infos")
         return None
 
-
+#checking if the 2 correct arguments are given properly
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python library.py <library.txt>")
+        print("Usage: library.py <library.txt>")
         sys.exit(1)
 
     file_path = sys.argv[1]
     books = load_library(file_path)
 
+# Questionnaire to the CMD where you are given 3 choises
+#Explained in README 
     while True:
         print("\n1) Add Book  2) Show Library  Q) Quit")
         choice = input("Choose an option: ").strip().lower()
